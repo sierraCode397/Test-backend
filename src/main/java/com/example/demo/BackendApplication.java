b0209da3ddffe67616a1f2b340cd77d96790d357
izaack107 <izaack107@gmail.com>
2025-06-10 11:24:16 -0600

    Update

--- src/main/java/com/example/demo/BackendApplication.java
+++ src/main/java/com/example/demo/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,15 +28,6 @@ public class BackendApplication {
    * @param args command line arguments (not used).
    */
   public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-    System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-    System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-    System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-    System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
-    System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
-    System.setProperty("server.port", dotenv.get("PORT", "8080"));
-
     SpringApplication.run(BackendApplication.class, args);
   }
 
